78f85cf1fc27348b4a7af070285c9a4d64babb7f
Alexander <alexitorjlanus@gmail.com>
2023-10-05 20:53:29 -0300

    agregue gets

--- app.js
+++ app.js
@@ -5,18 +5,26 @@ const path = require("path");
 app.listen(3030, () => console.log("Levantando servidor en 3030"));
 
 app.get("/",(req,res)=>
-res.sendFile() )
+{let resolve=path.resolve(__dirname,"/views/index.html")
+res.sendFile(resolve)} )
 app.get("/babbage",(req,res)=>
-res.sendFile() )
+{let resolve=path.resolve(__dirname,"/views/babbage.html")
+res.sendFile(resolve)} )
 app.get("/berners-lee",(req,res)=>
-res.sendFile() )
+{let resolve=path.resolve(__dirname,"/views/berners-lee.html")
+res.sendFile(resolve)} )
 app.get("/clarke",(req,res)=>
-res.sendFile() )
+{let resolve=path.resolve(__dirname,"/clarke.html")
+res.sendFile(resolve)} )
 app.get("/hamilton",(req,res)=>
-res.sendFile() )
+{let resolve=path.resolve(__dirname,"/hamilton.html")
+res.sendFile(resolve)} )
 app.get("/hopper",(req,res)=>
-res.sendFile() )
+{let resolve=path.resolve(__dirname,"/hopper.html")
+res.sendFile(resolve)} )
 app.get("/lovelace",(req,res)=>
-res.sendFile() )
+{let resolve=path.resolve(__dirname,"/lovelace.html")
+res.sendFile(resolve) })
 app.get("/turing",(req,res)=>
-res.sendFile() )
+{let resolve=path.resolve(__dirname,"/turing.html")
+res.sendFile(resolve)} )
